442afec253840748d1bb54034dcf41769cc5654d
neelSchool <144788333+neelSchool@users.noreply.github.com>
2024-01-27 16:36:43 +0000

    commit

--- PersonalBankProject/Program.cs
+++ PersonalBankProject/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class PersonalBankProject
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("hello world");
+    }
+}
